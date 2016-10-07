d0e06b778bf9bad7bea9e4056424afe3a4ffb4b4
planetchili <planetchili@users.noreply.github.com>
2016-10-07 22:06:32 +0900

    CHOCOLATE STARFISH

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -20,12 +20,22 @@
 ******************************************************************************************/
 #include "MainWindow.h"
 #include "Game.h"
+#include "Mat2.h"
 
 Game::Game( MainWindow& wnd )
 	:
 	wnd( wnd ),
 	gfx( wnd )
 {
+	const float dTheta = 2.0f * PI / float( nflares * 2 );
+	for( int i = 0; i < nflares * 2; i++ )
+	{
+		const float rad = (i % 2 == 0) ? radOuter : radInner;
+		star.emplace_back(
+			rad * cos( float( i ) * dTheta ),
+			rad * sin( float( i ) * dTheta )
+		);
+	}
 }
 
 void Game::Go()
@@ -38,8 +48,25 @@ void Game::Go()
 
 void Game::UpdateModel()
 {
+	if( !wnd.kbd.KeyIsPressed( VK_SPACE ) )
+	{
+		theta += vRot;
+	}
 }
 
 void Game::ComposeFrame()
 {
+	const Vec2 trl = { float( gfx.ScreenWidth ) / 2.0f,float( gfx.ScreenHeight ) / 2.0f };
+	const Mat2 trf = Mat2::Rotation( theta ) * Mat2::Scaling( size );
+	auto vtx( star );
+	for( auto& v : vtx )
+	{
+		v *= trf;
+		v += trl;
+	}
+	for( auto i = vtx.cbegin(),end = std::prev( vtx.cend() ); i != end; i++ )
+	{
+		gfx.DrawLine( *i,*std::next( i ),Colors::White );
+	}
+	gfx.DrawLine( vtx.front(),vtx.back(),Colors::White );
 }

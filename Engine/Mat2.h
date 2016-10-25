3ae15c28e00c25f4b1e92167c6e9d907d796375d
planetchili <planetchili@users.noreply.github.com>
2016-10-25 17:14:34 +0900

    fixing order of vec2 mat2 multiplication

--- Engine/Mat2.h
+++ Engine/Mat2.h
@@ -98,8 +98,8 @@ template<typename T>
 _Vec2<T> operator*( const _Vec2<T>& lhs,const _Mat2<T>& rhs )
 {
 	return { 
-		lhs.x * rhs.elements[0][0] + lhs.y * rhs.elements[0][1],
-		lhs.x * rhs.elements[1][0] + lhs.y * rhs.elements[1][1]
+		lhs.x * rhs.elements[0][0] + lhs.y * rhs.elements[1][0],
+		lhs.x * rhs.elements[0][1] + lhs.y * rhs.elements[1][1]
 	};
 }
 

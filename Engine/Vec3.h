86accd3eb9dbd41cff6f939c2c40010b4f710590
Chili <Chili@users.noreply.github.com>
2017-01-23 23:47:58 +0900

    cross product added to Vec3

--- Engine/Vec3.h
+++ Engine/Vec3.h
@@ -111,6 +111,13 @@ public:
 	{
 		return _Vec3( *this ) *= rhs;
 	}
+	_Vec3	operator%( const _Vec3& rhs ) const
+	{
+		return _Vec3(
+			y * rhs.z - z * rhs.y,
+			z * rhs.x - x * rhs.z,
+			x * rhs.y - y * rhs.x );
+	}
 	_Vec3&	operator/=( const T &rhs )
 	{
 		x /= rhs;

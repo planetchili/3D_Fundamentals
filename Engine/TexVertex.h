7943bca8fd8460a9bf959cb7ad3bc80ab6178b01
Chili <Chili@users.noreply.github.com>
2017-02-13 19:28:51 +0900

    float top tex unified interpolation

--- Engine/TexVertex.h
+++ Engine/TexVertex.h
@@ -13,11 +13,52 @@ public:
 	{}
 	TexVertex InterpolateTo( const TexVertex& dest,float alpha ) const
 	{
-		return {
+		return{
 			pos.InterpolateTo( dest.pos,alpha ),
 			tc.InterpolateTo( dest.tc,alpha )
 		};
 	}
+	TexVertex& operator+=( const TexVertex& rhs )
+	{
+		pos += rhs.pos;
+		tc += rhs.tc;
+		return *this;
+	}
+	TexVertex operator+( const TexVertex& rhs ) const
+	{
+		return TexVertex( *this ) += rhs;
+	}
+	TexVertex& operator-=( const TexVertex& rhs )
+	{
+		pos -= rhs.pos;
+		tc -= rhs.tc;
+		return *this;
+	}
+	TexVertex operator-( const TexVertex& rhs ) const
+	{
+		return TexVertex( *this ) -= rhs;
+	}
+	TexVertex& operator*=( float rhs )
+	{
+		pos *= rhs;
+		tc *= rhs;
+		return *this;
+	}
+	TexVertex operator*( float rhs ) const
+	{
+		return TexVertex( *this ) *= rhs;
+	}
+	TexVertex& operator/=( float rhs )
+	{
+		pos /= rhs;
+		tc /= rhs;
+		return *this;
+	}
+	TexVertex operator/( float rhs ) const
+	{
+		return TexVertex( *this ) /= rhs;
+	}
+public:
 	Vec3 pos;
 	Vec2 tc;
 };

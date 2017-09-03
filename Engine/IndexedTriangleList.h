6f371613021282549d95c66648ba36270ef8d8db
Chili <Chili@users.noreply.github.com>
2017-09-03 15:39:04 +0900

    remove culling flags from trilist

--- Engine/IndexedTriangleList.h
+++ Engine/IndexedTriangleList.h
@@ -14,9 +14,7 @@ public:
 	{
 		assert( vertices.size() > 2 );
 		assert( indices.size() % 3 == 0 );
-		cullFlags.resize( indices.size() / 3,false );
 	}
 	std::vector<T> vertices;
 	std::vector<size_t> indices;
-	std::vector<bool> cullFlags;
 };

397c5bdf2c3a9bfb42c8fe00416512db6ddb77d3
Chili <Chili@users.noreply.github.com>
2018-04-22 18:34:08 +0900

    add normals for sphere vertices

--- Engine/Sphere.h
+++ Engine/Sphere.h
@@ -24,6 +24,14 @@ public:
 				vertices.back().pos = latBase * Mat3::RotationZ( longitudeAngle * iLong );
 			}
 		}
+
+		// add the cap vertices
+		const auto iNorthPole = vertices.size();
+		vertices.emplace_back();
+		vertices.back().pos = base;
+		const auto iSouthPole = vertices.size();
+		vertices.emplace_back();
+		vertices.back().pos = -base;
 		
 		const auto calcIdx = [latDiv,longDiv]( int iLat,int iLong )
 			{ return iLat * longDiv + iLong; };
@@ -39,11 +47,48 @@ public:
 				indices.push_back( calcIdx( iLat + 1,iLong ) );
 				indices.push_back( calcIdx( iLat + 1,iLong + 1 ) );
 			}
-			// wrapping quads
+			// wrap band
+			indices.push_back( calcIdx( iLat,longDiv - 1 ) );
+			indices.push_back( calcIdx( iLat + 1,longDiv - 1 ) );
+			indices.push_back( calcIdx( iLat,0 ) );
+			indices.push_back( calcIdx( iLat,0 ) );
+			indices.push_back( calcIdx( iLat + 1,longDiv - 1 ) );
+			indices.push_back( calcIdx( iLat + 1,0 ) );			
 		}
 
 		// cap fans
+		for( int iLong = 0; iLong < longDiv - 1; iLong++ )
+		{
+			// north
+			indices.push_back( iNorthPole );
+			indices.push_back( calcIdx( 0,iLong ) );
+			indices.push_back( calcIdx( 0,iLong + 1 ) );
+			// south
+			indices.push_back( calcIdx( latDiv - 2,iLong + 1 ) );
+			indices.push_back( calcIdx( latDiv - 2,iLong ) );
+			indices.push_back( iSouthPole );
+		}
+		// wrap triangles
+		// north
+		indices.push_back( iNorthPole );
+		indices.push_back( calcIdx( 0,longDiv - 1 ) );
+		indices.push_back( calcIdx( 0,0 ) );
+		// south
+		indices.push_back( calcIdx( latDiv - 2,0 ) );
+		indices.push_back( calcIdx( latDiv - 2,longDiv - 1 ) );
+		indices.push_back( iSouthPole );
+
 
 		return{ std::move( vertices ),std::move( indices ) };
+	}
+	template<class V>
+	static IndexedTriangleList<V> GetPlainNormals( float radius = 1.0f,int latDiv = 12,int longDiv = 24 )
+	{
+		auto sphere = GetPlain<V>( radius,latDiv,longDiv );
+		for( auto& v : sphere.vertices )
+		{
+			v.n = v.pos.GetNormalized();
+		}
+		return sphere;
 	}
 };

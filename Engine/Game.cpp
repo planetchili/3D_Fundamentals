ada8c33f8ece68c097530fa2f39b7e0869a9fea9
Chili <Chili@users.noreply.github.com>
2018-08-20 03:43:00 +0900

    suzanne phong

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -40,6 +40,7 @@ Game::Game( MainWindow& wnd )
 	wnd( wnd ),
 	gfx( wnd )
 {
+	scenes.push_back( std::make_unique<PhongPointScene>( gfx,IndexedTriangleList<PhongPointScene::Vertex>::LoadNormals( "models\\suzanne.obj" ) ) );
 	scenes.push_back( std::make_unique<PhongPointScene>( gfx,Plane::GetNormals<PhongPointScene::Vertex>( 16 ) ) );
 	scenes.push_back( std::make_unique<GouraudPointScene>( gfx,Plane::GetNormals<GouraudPointScene::Vertex>( 16 ) ) );
 	scenes.push_back( std::make_unique<GouraudPointScene>( gfx,IndexedTriangleList<GouraudPointScene::Vertex>::LoadNormals( "models\\suzanne.obj" ) ) );
